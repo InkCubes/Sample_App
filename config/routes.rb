d9330eb849c606bc0ccb26584f0d53e3d07ddaa1
InkCube <birgit.klemenz@gmail.com>
2015-07-11 00:28:28 +0200

    Fill in links and tests for links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/help'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  get 'contact' => 'static_pages#contact'
 
   root 'static_pages#home'
 
